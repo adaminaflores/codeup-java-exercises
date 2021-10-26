13287b93a488083c9693791c8bbf77ab6f5a3e13
Adamina Flores <adaminaflores19@gmail.com>
2021-10-26 11:54:18 -0500

    Finished Teenage Bob exercise

--- src/Bob.java
+++ src/Bob.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Bob {
     public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Enter statement : ");
+        String userResponse = userInput.nextLine();
+            if(userResponse.endsWith("?")){
+                System.out.println("Sure.");
+            } if(userResponse.endsWith("!")){
+                System.out.println("Whoa, chill out!");
+            } if(userResponse.equals("")){
+                    System.out.println("Fine. Be that way!");
+            }else{
+                    System.out.println("Whatever.");
+            }
 
-    }
-}
+        }
+
+        }
+
+
