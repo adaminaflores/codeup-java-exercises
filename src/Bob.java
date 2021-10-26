34fa5544764b64d4ce07d000a31e5803b42294b6
Adamina Flores <adaminaflores19@gmail.com>
2021-10-26 13:54:47 -0500

    Went through review and made some adjustments

--- src/Bob.java
+++ src/Bob.java
@@ -4,19 +4,27 @@ public class Bob {
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
         System.out.println("Let's Talk to Bob: ");
+
         String userResponse = userInput.nextLine();
-            if(userResponse.endsWith("?")){
-                System.out.println("Sure.");
-            } if(userResponse.endsWith("!")){
-                System.out.println("Whoa, chill out!");
-            } if(userResponse.equals("")){
-                    System.out.println("Fine. Be that way!");
-            }else{
-                    System.out.println("Whatever.");
+
+            if (userResponse.endsWith("?")) {
+                System.out.println("Bob: Sure.");
+            }
+            if (userResponse.endsWith("!")) {
+                System.out.println("Bob: Whoa, chill out!");
+            }
+            if (userResponse.equals("")) {
+                System.out.println("Bob: Fine. Be that way!");
+            } else {
+                System.out.println("Bob: Whatever.");
+
             }
 
         }
 
-        }
+    }
+
+
+
 
 
