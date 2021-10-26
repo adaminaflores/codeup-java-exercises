97f6c94b91926f01de4627b5522480d422567f0c
Adamina Flores <adaminaflores19@gmail.com>
2021-10-26 15:11:40 -0500

    Finishing up first part of Methods Exercise

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,28 +1,36 @@
 import java.util.Scanner;
 
 public class MethodsExercises {
-    public static void calculate(int num1, int num2, String input) {
-    Scanner scanner= new Scanner(System.in);
-    System.out.println("What would you like to calculate?");
+    public static void userCalculate() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Pick two numbers, please!");
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        System.out.println("Would you like to add, subtract, multiply, divide, orrr modulus?");
+        String input = scanner.next();
 
-        switch (input) {
-            case "Addition":
-                System.out.println("Add: " + (num1 + num2));
-                // Fall through!
-            case "Subtraction":
-                System.out.println("Subtract: " + (num1 - num2));
-                break;
-            case "Multiplication":
-                System.out.println("Multiply: " + (num1 * num2));
-                break;
-            case "Division":
-                System.out.println("Divide: " + (num1 / num2));
-                break;
-            case "Modulus":
-                System.out.println("Modulus: " + (num1 % num2));
-                break;
-
-        }
+        calculate(num1, num2, input);
 
     }
-}
+        public static void calculate ( int num1, int num2, String input){
+            switch (input) {
+                case "Addition":
+                    System.out.println("Add: " + (num1 + num2));
+
+                case "Subtraction":
+                    System.out.println("Subtract: " + (num1 - num2));
+                    break;
+                case "Multiplication":
+                    System.out.println("Multiply: " + (num1 * num2));
+                    break;
+                case "Division":
+                    System.out.println("Divide: " + (num1 / num2));
+                    break;
+                case "Modulus":
+                    System.out.println("Modulus: " + (num1 % num2));
+                    break;
+
+            }
+
+        }
+    }
