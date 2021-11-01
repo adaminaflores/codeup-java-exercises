96e51cc241c78efdb454c4dcb04c43b6dee39a46
Adamina Flores <adaminaflores19@gmail.com>
2021-11-01 13:57:01 -0500

    Part 1 of Arrays exercise completed

--- src/ArraysExercise.java
+++ src/ArraysExercise.java
@@ -1,8 +1,32 @@
 import java.util.Arrays;
 
 public class ArraysExercise {
-    String[] Person = {"Addie", "Jackie", "Ian"};
+    public static Person[] addNewPerson(Person[] Person, Person name){
+        Person[] newPerson = Arrays.copyOf(Person , Person.length +1 );
+        newPerson[newPerson.length -1] = name;
+        return newPerson;
+
+        }
+
+    public static void main(String[] args) {
+        Person[] Person = new Person[3];
+        Person[0] = new Person("Addie");
+        Person[1] = new Person("Ian");
+        Person[2] = new Person("Jackie");
+        Person Nick = new Person("Nick");
+
+
+        Person = addNewPerson(Person, Nick);
+        for(Person name : Person){
+            System.out.println(name.getName());
+        }
 
 
 
-}
+    }
+
+    }
+
+
+
+
