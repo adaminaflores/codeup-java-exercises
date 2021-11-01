a1a725144384cbf8714c6e0bb70628a75c6b6e57
Adamina Flores <adaminaflores19@gmail.com>
2021-11-01 09:05:39 -0500

    Person exercise almost complete

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,4 +1,38 @@
 package util;
 
+import java.util.Scanner;
+
 public class Input {
+
+    private Scanner scanner;
+
+    public static void main(String[] args) {
+     Scanner scanner = new Scanner(System.in);
+      Input input = new Input();
+
+    }
+
+    public String getString(){
+        return scanner.nextLine();
+
+    }
+    public boolean yesNo(){
+
+    }
+
+    public int getInt(int min, int max){
+
+    }
+
+    public int getInt(){
+
+    }
+
+    public double getDouble(double min, double max){
+
+    }
+
+    public double getDouble(){
+
+    }
 }
