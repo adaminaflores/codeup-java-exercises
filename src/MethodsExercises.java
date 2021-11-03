dcc6446c2c4ad5c12c9b3a9737bd1ac2fee3131f
Adamina Flores <adaminaflores19@gmail.com>
2021-11-03 10:18:49 -0500

    Finishing methods exercise and movie part of arrays exercise

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -5,6 +5,10 @@ public class MethodsExercises {
 //        userCalculate();
         System.out.println("Enter a number between 1 and 10: ");
         int userInput = getInteger(1, 10);
+        System.out.println(getInteger(1,10));
+        factorial();
+
+
     }
     public static void userCalculate() {
         Scanner scanner = new Scanner(System.in);
@@ -48,5 +52,50 @@ public class MethodsExercises {
            System.out.printf("Enter a number between %d - %d: \n", min, max);
            return getInteger(min, max);
        }
+
     }
+
+    public static void factorial(){
+        Scanner scanner = new Scanner(System.in);
+        int userInput;
+        String isContinue;
+        long factorial = 1;
+        String middle = "1";
+
+        do{
+            userInput = getInteger(1,10);
+            for(int i = 1; i <= userInput; i++){
+                if(i > 1){
+                    middle += "x" + i;
+                }
+                factorial *= i;
+                System.out.printf("%2d! = %-45s = %d%n", i, middle, factorial);
+            }
+            System.out.println("Would you like to continue? ");
+                isContinue = scanner.nextLine();
+
+        }while(isContinue.equalsIgnoreCase("y"));
+
     }
+    public void  dice() {
+        Scanner sc = new Scanner(System.in);
+        String answer;
+        System.out.println("How many sides would you like this dice to have?");
+        int sides = sc.nextInt();
+        System.out.println("Would you like to roll the dice?");
+        String choice = sc.nextLine();
+        if (choice.equalsIgnoreCase("y")) {
+            do {
+                int dice1 = (int) Math.floor(Math.random() + sides);
+                int dice2 = (int) Math.floor(Math.random() + sides);
+                System.out.println(dice1);
+                System.out.println(dice2);
+
+                System.out.println("Would you like to roll the dice again?");
+                answer = sc.nextLine();
+            } while (answer.equalsIgnoreCase("y"));
+
+        }
+
+
+    }}
--- src/movies/Movie.java
+++ src/movies/Movie.java
@@ -4,13 +4,23 @@ public class Movie {
     private String name;
     private String category;
 
-    public void movieList(String name, String category) {
+    public Movie(String name, String category) {
         this.name = name;
         this.category = category;
+    }
+    public String getName(){
+        return this.name;
 
+    }
+    public void setName(String name){
+        this.name = name;
+    }
 
-
-
+    public String getCategory() {
+        return category;
+    }
+    public void setCategory(String category){
+        this.category = category;
     }
 }
 
