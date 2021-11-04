387be0227114a0189ecaeaf01d4b36c2d1da6df7
Adamina Flores <adaminaflores19@gmail.com>
2021-11-04 16:21:39 -0500

    Added comments for later use

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -5,8 +5,11 @@ public class ShapesTest {
         Measurable myShape = new Square(9);
         Measurable myShape2 = new Rectangle(2,5);
 
+        //Square:
         System.out.println("myShape.getPerimeter() = " + myShape.getPerimeter());
         System.out.println("myShape.getArea() = " + myShape.getArea());
+
+        //Rectangle:
         System.out.println("myShape2.getPerimeter() = " + myShape2.getPerimeter());
         System.out.println("myShape2.getArea() = " + myShape2.getArea());
 
